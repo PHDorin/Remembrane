9b70266968903850d9ea7f19b12295147f877d2f
PHDorin <hanshirosan@gmail.com>
2016-10-17 13:14:40 -0700

    Added a sign in page

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var port = process.env.PORT || 3000;
+
+app.set('views', __dirname);
+app.engine('html', require('ejs'));
+app.set('view engine', 'html');
+app.use(express.static(__dirname));
+app.use(bodyParser.json())
+
+app.get('/', function(req,res){
+  res.render('signin');
+})
+
+
+
+
+
+
+
+
 
 app.listen(port, function(){
   console.log('the remembrane is watching');
