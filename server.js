df9a2a171967be66144130b0cfc5a649cb98f547
PHDorin <hanshirosan@gmail.com>
2016-10-17 13:53:06 -0700

    Still adding stuff for serving static

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var port = process.env.PORT || 3000;
 
-app.set('views', __dirname);
-app.engine('html', require('ejs'));
-app.set('view engine', 'html');
+// app.set('views', __dirname);
+// app.engine('html', require('ejs'));
+// app.set('view engine', 'html');
 app.use(express.static(__dirname));
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 
 // app.get('/', function(req,res){
 //   res.render('signin');
